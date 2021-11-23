c328edce6d81f055ddafc10066c7b4b67c6cd2ec
Nikolay Beketov <nbek@miko.ru>
2021-11-23 16:48:56 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/en/

--- Messages/en.php
+++ Messages/en.php
@@ -1,17 +1,15 @@
 <?php
-/**
+return [
+    /**
  * Copyright (C) MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Nikolay Beketov, 6 2018
  *
  */
-
-return [
-	'repModuleGetSsl'       => 'Lets Encrypt - %repesent%',
-	'mo_ModuleModuleGetSsl' => 'Lets Encrypt GetSSL',
-    'BreadcrumbModuleGetSsl'=> 'Lets Encrypt GetSSL',
-    'SubHeaderModuleGetSsl' => 'SSL for HTTPS',
-    'module_template_AddNewRecord'  => 'Add new',
-
+    'repModuleGetSsl' => 'Lets Encrypt - %repesent%',
+    'mo_ModuleModuleGetSsl' => 'Lets Encrypt GetSSL',
+    'BreadcrumbModuleGetSsl' => 'Lets Encrypt GetSSL',
+    'SubHeaderModuleGetSsl' => 'SSL certificates for HTTPS',
+    'module_template_AddNewRecord' => 'Add new',
 ];
