59225e40676285f121c8484cb8b5e9b249c82840
Weblate <noreply@weblate.org>
2022-06-03 12:57:26 +0300

    Added translation using Weblate (Greek)

---
+++ Messages/el.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModuleGetSsl' => 'Lets Encrypt - %repesent%',
+    'mo_ModuleModuleGetSsl' => 'Lets Encrypt Get ssl',
+    'BreadcrumbModuleGetSsl' => 'Lets Encrypt Get ssl',
+    'SubHeaderModuleGetSsl' => 'SSL сертификаты для HTTPS',
+    'module_template_AddNewRecord' => 'Добавить',
+];
