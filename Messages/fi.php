c4e5c68dd5cab057897b7935dc9273d7ba1cd801
Anonymous <noreply@weblate.org>
2024-09-06 04:00:26 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -7,9 +7,9 @@ return [
  * Written by Nikolay Beketov, 6 2018
  *
  */
-    'repModuleGetSsl' => '',
-    'mo_ModuleModuleGetSsl' => '',
-    'BreadcrumbModuleGetSsl' => '',
-    'SubHeaderModuleGetSsl' => '',
-    'module_template_AddNewRecord' => '',
+    'repModuleGetSsl' => 'Salataan - %repesent%',
+    'mo_ModuleModuleGetSsl' => 'Sallii salauksen Get ssl',
+    'BreadcrumbModuleGetSsl' => 'Sallii salauksen Get ssl',
+    'SubHeaderModuleGetSsl' => 'SSL-varmenteet HTTPS:lle',
+    'module_template_AddNewRecord' => 'Lisätä',
 ];
