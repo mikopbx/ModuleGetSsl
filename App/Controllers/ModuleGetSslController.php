2f21fb127d610738fe7290c64c4ce4947768b466
Nikolay Beketov <nbek@miko.ru>
2024-09-30 16:39:19 +0700

    Update for PHP 8.3

--- App/Controllers/ModuleGetSslController.php
+++ App/Controllers/ModuleGetSslController.php
@@ -1,19 +1,27 @@
 <?php
-/**
- * Copyright © MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Alexey Portnov, 11 2018
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
+
 namespace Modules\ModuleGetSsl\App\Controllers;
 use MikoPBX\AdminCabinet\Controllers\BaseController;
-use MikoPBX\Common\Models\CallQueues;
-use MikoPBX\Common\Models\Extensions;
 use MikoPBX\Modules\PbxExtensionUtils;
 use Modules\ModuleGetSsl\App\Forms\ModuleGetSslForm;
 use Modules\ModuleGetSsl\Models\ModuleGetSsl;
-use MikoPBX\Common\Models\Providers;
-use Modules\ModuleGetSsl\Models\PhoneBook;
 
 class ModuleGetSslController extends BaseController
 {
@@ -31,59 +39,6 @@ class ModuleGetSslController extends BaseController
         parent::initialize();
     }
 
-    public function getTablesDescriptionAction(): void
-    {
-        $this->view->data = $this->getTablesDescription();
-    }
-
-    public function getNewRecordsAction(): void
-    {
-        $currentPage                 = $this->request->getPost('draw');
-        $table                       = $this->request->get('table');
-        $this->view->draw            = $currentPage;
-        $this->view->recordsTotal    = 0;
-        $this->view->recordsFiltered = 0;
-        $this->view->data            = [];
-
-        $descriptions = $this->getTablesDescription();
-        if(!isset($descriptions[$table])){
-            return;
-        }
-        $className = $this->getClassName($table);
-        if(!empty($className)){
-            $filter = [];
-            if(isset($descriptions[$table]['cols']['priority'])){
-                $filter = ['order' => 'priority'];
-            }
-            $allRecords = $className::find($filter)->toArray();
-            $records    = [];
-            $emptyRow   = [
-                'rowIcon'  =>  $descriptions[$table]['cols']['rowIcon']['icon']??'',
-                'DT_RowId' => 'TEMPLATE'
-            ];
-            foreach ($descriptions[$table]['cols'] as $key => $metadata) {
-                if('rowIcon' !== $key){
-                    $emptyRow[$key] = '';
-                }
-            }
-            $records[] = $emptyRow;
-            foreach ($allRecords as $rowData){
-                $tmpData = [];
-                $tmpData['DT_RowId'] =  $rowData['id'];
-                foreach ($descriptions[$table]['cols'] as $key => $metadata){
-                    if('rowIcon' === $key){
-                        $tmpData[$key] = $metadata['icon']??'';
-                    }elseif('delButton' === $key){
-                        $tmpData[$key] = '';
-                    }elseif(isset($rowData[$key])){
-                        $tmpData[$key] =  $rowData[$key];
-                    }
-                }
-                $records[] = $tmpData;
-            }
-            $this->view->data      = $records;
-        }
-    }
 
     public function getCertAction():void{
         $this->view->data = ['test'];
@@ -95,33 +50,18 @@ class ModuleGetSslController extends BaseController
     {
         $footerCollection = $this->assets->collection('footerJS');
         $footerCollection->addJs('js/pbx/main/form.js', true);
-        $footerCollection->addJs('js/vendor/datatable/dataTables.semanticui.js', true);
         $footerCollection->addJs("js/cache/{$this->moduleUniqueID}/module-get-ssl-index.js", true);
-        $footerCollection->addJs('js/vendor/jquery.tablednd.min.js', true);
 
         $headerCollectionCSS = $this->assets->collection('headerCSS');
         $headerCollectionCSS->addCss("css/cache/{$this->moduleUniqueID}/module-get-ssl.css", true);
-        $headerCollectionCSS->addCss('css/vendor/datatable/dataTables.semanticui.min.css', true);
 
         $settings = ModuleGetSsl::findFirst();
         if ($settings === null) {
             $settings = new ModuleGetSsl();
         }
 
-        // For example we add providers list on the form
-        $providers = Providers::find();
-        $providersList = [];
-        foreach ($providers as $provider){
-            $providersList[ $provider->uniqid ] = $provider->getRepresent();
-        }
-        $options['providers']=$providersList;
-
-        $this->view->form = new ModuleGetSslForm($settings, $options);
+        $this->view->form = new ModuleGetSslForm($settings, []);
         $this->view->pick("{$this->moduleDir}/App/Views/index");
-
-        // Список выбора очередей.
-        $this->view->queues = CallQueues::find(['columns' => ['id', 'name']]);
-        $this->view->users  = Extensions::find(["type = 'SIP'", 'columns' => ['number', 'callerid']]);
     }
 
     /**
@@ -169,137 +109,4 @@ class ModuleGetSslController extends BaseController
         $this->db->commit();
     }
 
-    /**
-     * Delete phonebook record
-     */
-    public function deleteAction(): void
-    {
-        $table     = $this->request->get('table');
-        $className = $this->getClassName($table);
-        if(empty($className)) {
-            $this->view->success = false;
-            return;
-        }
-        $id     = $this->request->get('id');
-        $record = $className::findFirstById($id);
-        if ($record !== null && ! $record->delete()) {
-            $this->flash->error(implode('<br>', $record->getMessages()));
-            $this->view->success = false;
-            return;
-        }
-        $this->view->success = true;
-    }
-
-    /**
-     * Возвращает метаданные таблицы.
-     * @return array
-     */
-    private function getTablesDescription():array
-    {
-        $description['PhoneBook'] = [
-            'cols' => [
-                'rowIcon'    => ['header' => '',                        'class' => 'collapsing', 'icon' => 'user'],
-                'priority'   => ['header' => '',                        'class' => 'collapsing'],
-                'call_id'    => ['header' => 'Представление абонента',  'class' => 'ten wide'],
-                'number_rep' => ['header' => 'Номер телефона',          'class' => 'four wide'],
-                'queueId'    => ['header' => 'Номер числом',            'class' => 'collapsing', 'select' => 'queues-list'],
-                'delButton'  => ['header' => '',                        'class' => 'collapsing']
-            ],
-            'ajaxUrl' => '/getNewRecords',
-            'icon' => 'user',
-            'needDelButton' => true
-        ];
-        return $description;
-    }
-
-    /**
-     * Обновление данных в таблице.
-     */
-    public function saveTableDataAction():void
-    {
-        $data       = $this->request->getPost();
-        $tableName  = $data['pbx-table-id']??'';
-
-        $className = $this->getClassName($tableName);
-        if(empty($className)){
-            return;
-        }
-        $rowId      = $data['pbx-row-id']??'';
-        if(empty($rowId)){
-            $this->view->success = false;
-            return;
-        }
-        $this->db->begin();
-        /** @var PhoneBook $rowData */
-        $rowData = $className::findFirst('id="'.$rowId.'"');
-        if(!$rowData){
-            $rowData = new $className();
-        }
-        foreach ($rowData as $key => $value) {
-            if($key === 'id'){
-                continue;
-            }
-            if (array_key_exists($key, $data)) {
-                $rowData->writeAttribute($key, $data[$key]);
-            }
-        }
-        // save action
-        if ($rowData->save() === FALSE) {
-            $errors = $rowData->getMessages();
-            $this->flash->error(implode('<br>', $errors));
-            $this->view->success = false;
-            $this->db->rollback();
-            return;
-        }
-        $this->view->data = ['pbx-row-id'=>$rowId, 'newId'=>$rowData->id, 'pbx-table-id' => $data['pbx-table-id']];
-        $this->view->success = true;
-        $this->db->commit();
-
-    }
-
-    /**
-     * Получение имени класса по имени таблицы
-     * @param $tableName
-     * @return string
-     */
-    private function getClassName($tableName):string
-    {
-        if(empty($tableName)){
-            return '';
-        }
-        $className = "Modules\ModuleGetSsl\Models\\$tableName";
-        if(!class_exists($className)){
-            $className = '';
-        }
-        return $className;
-    }
-
-    /**
-     * Changes rules priority
-     *
-     */
-    public function changePriorityAction(): void
-    {
-        $this->view->disable();
-        $result = true;
-
-        if ( ! $this->request->isPost()) {
-            return;
-        }
-        $priorityTable = $this->request->getPost();
-        $tableName     = $this->request->get('table');
-        $className = $this->getClassName($tableName);
-        if(empty($className)){
-            echo "table not found -- ы$tableName --";
-            return;
-        }
-        $rules = $className::find();
-        foreach ($rules as $rule){
-            if (array_key_exists ( $rule->id, $priorityTable)){
-                $rule->priority = $priorityTable[$rule->id];
-                $result         .= $rule->update();
-            }
-        }
-        echo json_encode($result);
-    }
 }
--- Lib/RestAPI/Controllers/GetController.php
+++ Lib/RestAPI/Controllers/GetController.php
@@ -1,25 +1,25 @@
 <?php
 /*
- * Copyright © MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Alexey Portnov, 8 2020
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace Modules\ModuleGetSsl\Lib\RestAPI\Controllers;
 
-use MikoPBX\AdminCabinet\Providers\SessionProvider;
-use MikoPBX\Common\Models\CallQueueMembers;
-use MikoPBX\Common\Models\CallQueues;
-use MikoPBX\Common\Models\Extensions;
-use MikoPBX\Common\Providers\BeanstalkConnectionModelsProvider;
-use MikoPBX\Common\Providers\MessagesProvider;
-use MikoPBX\Common\Providers\TranslationProvider;
-use MikoPBX\Modules\PbxExtensionUtils;
-use MikoPBX\PBXCoreREST\Controllers\BaseController;
 use MikoPBX\PBXCoreREST\Controllers\Modules\ModulesControllerBase;
-use Modules\ModuleAdditionalWebAPI\Lib\ModuleAdditionalWebAPIWorker;
-use Phalcon\Di;
 
 class GetController extends ModulesControllerBase
 {
