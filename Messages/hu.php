35a399bda832f4b1356050694756a4601fe4129b
Anonymous <noreply@weblate.org>
2024-10-10 09:30:09 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -11,4 +11,10 @@ return [
     'mo_ModuleModuleGetSsl' => 'Lets Encrypt Get ssl',
     'BreadcrumbModuleGetSsl' => 'Lets Encrypt Get ssl',
     'SubHeaderModuleGetSsl' => 'SSL-tanúsítványok HTTPS-hez',
+    'module_getssl_DomainNameLabel' => 'Domain név http és https nélkül, csak a név',
+    'module_getssl_autoUpdateLabel' => 'Tanúsítvány automatikus megújítása',
+    'module_getssl_getUpdateStatusText' => 'Tanúsítvány kérése a Lets Encrypt alkalmazásban',
+    'module_getssl_DomainNameEmpty' => 'Adja meg a tartomány értékét a tanúsítvány létrehozásához',
+    'module_getssl_getUpdateSSLButton' => 'SSL-tanúsítvány beszerzése/újítása',
+    'module_getssl_getUpdateStatusHeader' => 'Folyamatban van',
 ];
