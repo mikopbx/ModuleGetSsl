e53fbb0c389b6adcf01fa6d2fc768856bda73c45
Anonymous <noreply@weblate.org>
2024-10-14 08:24:25 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/fi/

--- Messages/fi.php
+++ Messages/fi.php
@@ -15,4 +15,9 @@ return [
     'module_getssl_autoUpdateLabel' => 'Uusi varmenne automaattisesti',
     'module_getssl_getUpdateSSLButton' => 'Hanki/uusi SSL-varmenne',
     'module_getssl_DomainNameEmpty' => 'Syötä verkkotunnuksen arvo luodaksesi varmenteen',
+    'module_getssl_GetSSLProcessing' => 'Pyydetään tietoja Lets Encryptissä...',
+    'module_getssl_GetSSLProcessingTimeout' => 'Virhe, Lets Encrypt -palvelu ei vastannut 2 minuutin kuluessa',
+    'module_getssl_getUpdateLogHeader' => 'Varmennepyynnön tulos Lets Encryptissä',
+    'module_getssl_ConfigStartsGenerating' => 'Luodaan määritystiedostoja...',
+    'module_getssl_ConfigGenerated' => 'Asetustiedostot luotu...',
 ];
