4ecc78a444041721c4fd366973abfcde1616df59
Anonymous <noreply@weblate.org>
2024-10-14 08:24:19 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/zh_Hans/

--- Messages/zh_Hans.php
+++ Messages/zh_Hans.php
@@ -15,4 +15,9 @@ return [
     'module_getssl_autoUpdateLabel' => '自动更新证书',
     'module_getssl_getUpdateSSLButton' => '获取/续订 SSL 证书',
     'module_getssl_DomainNameEmpty' => '输入域名值生成证书',
+    'module_getssl_getUpdateLogHeader' => 'Lets Encrypt 中证书请求的结果',
+    'module_getssl_ConfigStartsGenerating' => '正在生成配置文件...',
+    'module_getssl_ConfigGenerated' => '配置文件已创建...',
+    'module_getssl_GetSSLProcessing' => '在 Lets Encrypt 中请求数据...',
+    'module_getssl_GetSSLProcessingTimeout' => '错误，Lets Encrypt 服务未在 2 分钟内返回响应',
 ];
