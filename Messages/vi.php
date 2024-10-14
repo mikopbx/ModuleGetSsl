d549d8cf57d11250e3eade5c0ed1aded369c1377
Anonymous <noreply@weblate.org>
2024-10-14 08:24:18 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -15,4 +15,9 @@ return [
     'module_getssl_autoUpdateLabel' => 'Tự động gia hạn chứng chỉ',
     'module_getssl_getUpdateSSLButton' => 'Nhận/gia hạn chứng chỉ SSL',
     'module_getssl_DomainNameEmpty' => 'Nhập giá trị tên miền để tạo chứng chỉ',
+    'module_getssl_getUpdateLogHeader' => 'Kết quả yêu cầu chứng chỉ trong Lets Encrypt',
+    'module_getssl_ConfigStartsGenerating' => 'Đang tạo tập tin cấu hình...',
+    'module_getssl_ConfigGenerated' => 'Các tập tin cấu hình được tạo...',
+    'module_getssl_GetSSLProcessing' => 'Đang yêu cầu dữ liệu trong Lets Encrypt...',
+    'module_getssl_GetSSLProcessingTimeout' => 'Lỗi, dịch vụ Lets Encrypt không phản hồi trong vòng 2 phút',
 ];
