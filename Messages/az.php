d16dbda473ed563a27097888af297183626fe322
Anonymous <noreply@weblate.org>
2024-10-14 08:24:23 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/az/

--- Messages/az.php
+++ Messages/az.php
@@ -15,4 +15,9 @@ return [
     'module_getssl_autoUpdateLabel' => 'Sertifikatı avtomatik yeniləyin',
     'module_getssl_DomainNameEmpty' => 'Sertifikat yaratmaq üçün domen dəyərini daxil edin',
     'module_getssl_getUpdateSSLButton' => 'SSL sertifikatı alın/təzələyin',
+    'module_getssl_getUpdateLogHeader' => 'Lets Encrypt-də sertifikat sorğusunun nəticəsi',
+    'module_getssl_ConfigStartsGenerating' => 'Konfiqurasiya faylları yaradılır...',
+    'module_getssl_ConfigGenerated' => 'Konfiqurasiya faylları yaradıldı...',
+    'module_getssl_GetSSLProcessing' => 'Lets Encrypt-də data tələb olunur...',
+    'module_getssl_GetSSLProcessingTimeout' => 'Xəta, Lets Encrypt xidməti 2 dəqiqə ərzində cavab qaytarmadı',
 ];
