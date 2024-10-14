188cd831626dd34dc057befb464b6aabf7592889
Anonymous <noreply@weblate.org>
2024-10-14 08:24:13 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MIKOPBX/ModuleGetSsl
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulegetssl/pl/

--- Messages/pl.php
+++ Messages/pl.php
@@ -15,4 +15,9 @@ return [
     'module_getssl_autoUpdateLabel' => 'Odnów certyfikat automatycznie',
     'module_getssl_DomainNameEmpty' => 'Wprowadź wartość domeny, aby wygenerować certyfikat',
     'module_getssl_getUpdateSSLButton' => 'Zdobądź/odnów certyfikat SSL',
+    'module_getssl_getUpdateLogHeader' => 'Wynik żądania certyfikatu w Lets Encrypt',
+    'module_getssl_ConfigStartsGenerating' => 'Generowanie plików konfiguracyjnych...',
+    'module_getssl_ConfigGenerated' => 'Utworzono pliki konfiguracyjne...',
+    'module_getssl_GetSSLProcessing' => 'Żądanie danych w Lets Encrypt...',
+    'module_getssl_GetSSLProcessingTimeout' => 'Błąd, usługa Lets Encrypt nie zwróciła odpowiedzi w ciągu 2 minut',
 ];
